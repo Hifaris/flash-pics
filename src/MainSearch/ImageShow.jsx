7a67c420143418ff39eeb4079a2b017d55ca7742
Hifaris <nattapongbe@gmail.com>
2024-12-25 14:16:33 +0700

    add usestatr

--- src/MainSearch/ImageShow.jsx
+++ src/MainSearch/ImageShow.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Watermark, Pagination } from "antd";
 import Loading from "../component/Loading";
