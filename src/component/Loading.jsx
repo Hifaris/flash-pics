be21b71db12a341a3a551d48d3600484d30ed2e9
Hifaris <nattapongbe@gmail.com>
2024-12-27 08:32:49 +0700

    update: skeleton second

--- src/component/Loading.jsx
+++ src/component/Loading.jsx
@@ -6,11 +6,7 @@ function SkeletonCard(){
 return(
             <div>
 
-            <Skeleton className="w-full h-64  rounded-full" />
-            {/* <div className="space-y-2">
-                <Skeleton className="h-4 w-[250px]" />
-                <Skeleton className="h-4 w-[200px]" />
-            </div> */}
+            <Skeleton className="w-full h-64 rounded-md" />
             </div>
 
 )
@@ -18,6 +14,10 @@ return(
 function Loading() {
     return (
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8 mt-5 ">
+            <SkeletonCard/>
+            <SkeletonCard/>
+            <SkeletonCard/>
+            <SkeletonCard/>
             <SkeletonCard/>
             <SkeletonCard/>
             <SkeletonCard/>
